123565401c53d15ed048cb57bc109feb4dcf78e7
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-11 09:32:31 -0700

    I did not get very far

---
+++ calculator.js
@@ -0,0 +1,36 @@
+// I was working with steph but when I go home I couldn't really get any further so I went
+// back to doing the exercises from yesterday
+window.onload = function () {
+
+var operatorDisplay = document.getElementById("operator_display");
+var resultDisplay = document.getElementById("result_display_value");
+
+var one = document.getElementById("one")
+var two = document.getElementById("two")
+var three = document.getElementById("three")
+var four = document.getElementById("four")
+var five = document.getElementById("five")
+var six = document.getElementById("six")
+var seven = document.getElementById("seven")
+var eight = document.getElementById("eight")
+var nine = document.getElementById("nine")
+var zero = document.getElementById("zero")
+
+var plus = document.getElementById("plus")
+var minus = document.getElementById("minus")
+var multiply = document.getElementById("multiply")
+var clr = document.getElementById("clear")
+var enter = document.getElementById("enter")
+var divide = document.getElementById("divide")
+
+var clearDisplay = function () {
+	result_display_value.innerHTML=" ";
+	resultDisplay.innerHTML = " ";
+
+}
+
+
+
+
+
+};
